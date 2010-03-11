681f6bcc414586d22d2d9a8424674e794c606634
Robert Rees <spam@shudderfix.com>
2010-03-11 21:02:38 +0000

    Loading Showdown

--- src/fleet_wiki/markdown.clj
+++ src/fleet_wiki/markdown.clj
@@ -1,13 +1,21 @@
 (ns fleet-wiki.markdown
-	(:import (javax.script ScriptEngineManager ScriptEngine)))
+	(:import (javax.script ScriptEngineManager ScriptEngine))
+	(:import (java.io InputStreamReader File FileReader)))
 	
+(defn load-showdown
+	[]
+	(let [showdown-file (new File "src/scripts/showdown.js")]
+	 (new FileReader showdown-file)))
+
 (defn to-html
 	[markdown]
 	(let
 		[sem (new ScriptEngineManager)
 		javascript (. sem getEngineByExtension "js")
+		showdown (load-showdown)
 		]
 		(. javascript eval "function makeHtml(input) { return input; }")
+		(. javascript eval showdown)
 		(println markdown)
 		(println (str "Output: " (. javascript invokeFunction "makeHtml" (to-array [markdown]))))))
 
