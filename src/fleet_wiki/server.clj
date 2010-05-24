e6bb3842eaebddce0aa93e2f7216535cbfc78801
Robert Rees <rrees@thoughtworkers-macbook-pro-7.local>
2010-05-24 12:17:26 +0100

    Pushed the routes into their own namespace

--- src/fleet_wiki/server.clj
+++ src/fleet_wiki/server.clj
@@ -1,12 +1,4 @@
 (ns fleet-wiki.server
-	(:use :reload-all compojure.core compojure.route fleet-wiki.handlers ring.adapter.jetty))
+	(:use :reload-all fleet-wiki.routes ring.adapter.jetty))
 
-(defroutes fleet-wiki
-	(GET "/" request
-		(show-front-page))
-	(GET "/wiki/:topic" [topic] (show-topic topic))
-	(POST "/wiki/:topic" [topic content] (update-topic topic content))
-	(GET "/wiki/:topic/edit" [topic] (edit-topic topic))
-	(compojure.route/files "/css" {:root "css"}))
-
-(run-jetty fleet-wiki {:port 9999})
+(run-jetty fleet-wiki.routes/app {:port 9999})
